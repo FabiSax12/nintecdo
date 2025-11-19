497a5a42b63e48777ce8491bfd4e43d6e560c1c6
Fabian Vargas <fabidev18@gmail.com>
2025-11-19 16:22:54 -0600

    Remove games from this repo

--- src/main/java/com/nintecdo/games/tiktaktoe/GameState.java
+++
@@ -1,5 +0,0 @@
-package com.nintecdo.games.tiktaktoe;
-
-public enum GameState {
-    IN_PROGRESS, X_WINS, O_WINS, DRAW
-}
--- src/main/java/com/nintecdo/games/tiktaktoe/InvalidMoveException.java
+++
@@ -1,7 +0,0 @@
-package com.nintecdo.games.tiktaktoe;
-
-public class InvalidMoveException extends Exception {
-    public InvalidMoveException(String message) {
-        super(message);
-    }
-}
